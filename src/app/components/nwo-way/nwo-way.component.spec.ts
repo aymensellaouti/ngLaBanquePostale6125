1e778be7499155508e4cf0a3d3cf9e7557c07dd8
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-06 13:42:55 +0100

    mise en place du two way exemple et de l'exercice rotating card

---
+++ src/app/components/nwo-way/nwo-way.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NwoWayComponent } from './nwo-way.component';
+
+describe('NwoWayComponent', () => {
+  let component: NwoWayComponent;
+  let fixture: ComponentFixture<NwoWayComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [NwoWayComponent]
+    });
+    fixture = TestBed.createComponent(NwoWayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/nwo-way/nwo-way.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nwo-way',
+  templateUrl: './nwo-way.component.html',
+  styleUrls: ['./nwo-way.component.css']
+})
+export class NwoWayComponent {
+  two = 'init value';
+}
