e63b30bbe4c1e361ad1e450dcc05381cf161ca8c
aymensellaouti <aymen.sellaouti@gmail.com>
2025-01-08 16:46:24 +0100

    update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, inject, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ngLaBanquePostale6125';
+  appRef = inject(ApplicationRef);
+
 }
